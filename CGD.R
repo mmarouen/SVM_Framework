0369c52d018f6860a10e8590c6a0cd541f4cea14
azzouz marouen <azzouz.marouen@gmail.com>
2017-12-11 14:22:51 +0100

    Update CGD.R

--- CGD.R
+++ CGD.R
@@ -1,15 +1,13 @@
 #conjugate gradient descent classifier
 
-CGD<-function(X, #input matrix
-              resp, #response matrix
-              XT,#test matrix
-              respT,#test response
-              Ker,H,S, #kernel matrices
-              classifier, #'SVM' or 'Softmax' classifier
-              lambd, #regularizer
-              tol, #tolerence value
-              epochs #n_iterations
-              ){
+CGD<-function(X,resp,XT,respT, #respectively train matrix, response, test matrix, response
+              Ker,kerT, #train kernel & test kernel
+              H,S, #adaptations of the kernel matrix
+              classifier, #classifier to be used: svm or softmax
+              lambd, #regularization parameter
+              tol, #tolerence factor
+              epochs #max iteration
+             ){
   N=nrow(resp)
   K=ncol(resp)
   betahat=c()
@@ -31,9 +29,8 @@ CGD<-function(X, #input matrix
     K1[1,1]=1
     yhat=H%*%betahat
     loss=c()
-    d=-(A%*%betahat-b)
+    d=2*(-A%*%betahat+b)
     g_old=d
-    gnorm0=sum(g_old^2)
     thd=tol*gnorm0
     repeat{
       alpha1=t(d)%*%K1
@@ -47,10 +44,9 @@ CGD<-function(X, #input matrix
       g_new=-A%*%betahat+b
       numerator=as.numeric(t(g_new)%*%K1%*%g_new)
       denumerator=as.numeric(t(g_old)%*%K1%*%g_old)
-      d=g_new+numerator*d/denumerator
-      gnorm1=sum(g_new^2)
+      d=2*g_new+numerator*d/denumerator
+      gnorm1=sum((K1%*%g_new)^2)
       if(t>50 | all(sv==sv1) | gnorm1<thd) break
-      
       t=t+1
       sv=sv1
       g_old=g_new
