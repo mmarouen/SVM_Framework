015d0e233fad7760a8b41c63ac52cc0e133710fc
azzouz marouen <azzouz.marouen@gmail.com>
2017-12-11 17:57:50 +0100

    Update NGD.R

--- NGD.R
+++ NGD.R
@@ -1,24 +1,18 @@
 #Newton-raphson optimization
-NGD<-function(X, #input data
-              resp, #response matrix
-              Ker, #kernel matrix
-              classifier, #'SVM' or 'Softmax' classifier
-              lambd, #regularizer=1/C
-              tol, #tolerence
-              epochs #n_iterations
-              ){
+NGD<-function(X,resp,Ker,classifier,lambd,tol,epochs){
   N=nrow(resp)
   K=ncol(resp)
   sv=rep(TRUE,N)
-   if(N>=1000){
-     N1=N%/%2
-     X1=X[1:N1,]
-     resp1=as.matrix(resp[1:N1,])
-     Ker1=Ker[1:N1,1:N1]
-     out=NGD(X1,resp1,Ker1,classifier,lambd,tol,epochs)
-     sv[1:N1]=out$sv
+  betahat=rep(0,N+1)
+  if(N>=1000){
+    N1=N%/%2
+    X1=X[1:N1,]
+    resp1=as.matrix(resp[1:N1,])
+    Ker1=Ker[1:N1,1:N1]
+    out=NGD(X1,resp1,Ker1,classifier,lambd,tol,epochs)
+    if(classifier=="SVM") sv[1:N1]=out$sv
+    if(classifier=="Softmax") betahat[1:N1]=out$betahat
   }
-  betahat=c()
   objCurve=c()
   t=1
   if(classifier=="SVM" & K==1){
@@ -31,6 +25,8 @@ NGD<-function(X, #input data
       IntMat=rbind(1,IntMat)
       IntMat=cbind(1,IntMat)
       IntMat[1,1]=0
+
+      # bsv=invMat(IntMat,eps = 1e-12)%*%Ysv
       bsv=solve(IntMat)%*%Ysv
       intercept=bsv[1]
       betahat[sv]=bsv[-1]
@@ -40,46 +36,39 @@ NGD<-function(X, #input data
       sv_new=(resp*yhat) <1
       if(all(sv_new==sv)|t>50) break
       sv=sv_new
-      print(paste(round(as.numeric(obj),3),sum(sv)))
+      # print(paste(round(as.numeric(obj),3),sum(sv)))
       t=t+1
     }
     betahat=c(intercept,betahat)
   }
   if(classifier=="Softmax" & K==1){
     obj1=0
+    H=cbind(1,Ker)
+    K1=cbind(0,Ker)
+    K1=rbind(0,K1)
+    K1[1,1]=1
+    I1=cbind(0,diag(N))
+    I1=rbind(0,I1)
+    I2=rbind(1,diag(N))
+    beta_old=betahat
+    f_x=H%*%beta_old
+    yhat=softmax(f_x)
+    grad=2*lambd*I1%*%beta_old+I2%*%(yhat-resp)
+    # thd=sum((K1%*%grad)^2)*tol
     repeat{
-      betahat_old=rep(0,N)
-      beta0_old=0
-      f_x=Ker%*%betahat_old+beta0_old
+      grad=2*lambd*I1%*%beta_old+I2%*%(yhat-resp)
+      hess=2*lambd*I1+I2%*%diag(c(yhat*(1-yhat)))%*%H
+      delt=solve(hess)%*%grad
+      beta_new=beta_old-delt
+      f_x=H%*%beta_new
       yhat=softmax(f_x)
-      obj=objective(classifier,betahat =betahat_old,kerMat = Ker,y = resp,f_x = yhat,lambd = lambd)
-      objCurve=c(objCurve,obj)
-      grad_b=Ker%*%(lambd*betahat_old+yhat-resp)
-      grad_b0=sum(yhat-resp)
-      grad=c(grad_b0,grad_b)
-      hessian_b=Ker%*%(lambd*diag(N)+diag(c(yhat*(1-yhat)))%*%Ker)
-      hessian_b0=sum(yhat*(1-yhat))
-      hessian_bb0=Ker%*%(yhat*(1-yhat))
-      hessian=matrix(ncol=N+1,nrow=N+1)
-      hessian[2:(N+1),2:(N+1)]=hessian_b
-      hessian[1,1]=hessian_b0
-      hessian[2:(N+1),1]=hessian_bb0
-      hessian[1,2:(N+1)]=t(hessian_bb0)
-      betahat=c(beta0_old,betahat_old)-invMat(hessian,eps = 1e-7)%*%grad
-      thd=abs(obj1-obj)
-      # if(t%%5==0){
-      #   print(paste(t,obj))
-      #   print(thd)
-      # }
-      if(thd<tol) break
-      # if(t==6) break
-      betahat_old=betahat[-1]
-      beta0_old=betahat[1]
       t=t+1
-      obj1=obj
+      eps=sum(grad*delt)
+      beta_old=beta_new
+      if(t>15| eps<tol) break
     }
     sv_new=0
   }
-  
-  return(list(betahat=betahat,n_iter=t,gradsErr=c(),lossCurve=objCurve,sv=sv_new))
+  return(list(betahat=beta_new,n_iter=t,gradsErr=c(),lossCurve=objCurve,sv=sv_new))
 }
+
